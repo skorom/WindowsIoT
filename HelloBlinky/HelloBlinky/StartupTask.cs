0aa8c3c8c0144679c76517da4ae91851962fffa3
skorom <korom.szilard@gmail.com>
2017-11-24 09:55:30 +0100

    Hello World projektek

---
+++ HelloBlinky/HelloBlinky/StartupTask.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using Windows.ApplicationModel.Background;
+using Windows.Devices.Gpio; //<------https://docs.microsoft.com/en-us/uwp/api/windows.devices.gpio
+
+// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+
+namespace HelloBlinky
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        private const int LED_PIN = 12;
+        private GpioPin pin;
+        private GpioPinValue pinValue;
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            InitGPIO();
+            if (pin != null)
+            {
+                if (pinValue == GpioPinValue.High)
+                {
+                    pinValue = GpioPinValue.Low;
+                    pin.Write(pinValue);
+                }
+                else
+                {
+                    pinValue = GpioPinValue.High;
+                    pin.Write(pinValue);
+                }
+            }
+        }
+        public void InitGPIO()
+        {
+            var gpio = GpioController.GetDefault();
+
+            if (gpio != null) //Tehát van GPIO vezérlő az eszközön
+            {
+                pin = gpio.OpenPin(LED_PIN);
+                pinValue = GpioPinValue.High;
+                pin.Write(pinValue);
+                pin.SetDriveMode(GpioPinDriveMode.Output);
+            }
+
+        }
+    }
+}
---
+++ ReadWrite/ReadWrite/StartupTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using Windows.ApplicationModel.Background;
+using Windows.Storage;
+
+
+namespace ReadWrite
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            FileKezeles();
+        }
+        public async void FileKezeles()
+        {
+            StorageFolder taroloMappa = ApplicationData.Current.LocalFolder;
+            StorageFile tesztFile = await taroloMappa.CreateFileAsync("sample.txt", 
+                                                    CreationCollisionOption.OpenIfExists);
+            await FileIO.WriteTextAsync(tesztFile, DateTime.Today.ToString());
+
+            string tesztTartalom = await FileIO.ReadTextAsync(tesztFile);
+
+        }
+    }
+}
